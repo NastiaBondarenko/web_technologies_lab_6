a8c196e6b9698f03d288f570414231f8d66cf159
nastiabondarenko17@gmail.com <nastiabondarenko17@gmail.com>
2022-10-27 00:38:31 +0300

    lab6

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-async function showPeople(){
-    let promise = await fetch('https://randomuser.me/api');
-    let json = await promise.json();
-    let blockForPeople = document.getElementById("blockForPeople");
-    let image = document.createElement('img');
-    image.src = json.results[0].picture.medium;
-    blockForPeople.append(image);
-    console.log(image);
+class Person{
+    constructor(result){
+
+        this.image = result.picture.large;
+        this.cell = result.cell;
+        this.city = result.location.city;
+        this.country = result.location.country;
+        this.postcode = result.location.postcode;
+    }
 }
 
-// main();
+
+async function getInformAboutPerson(){
+    let promise = await fetch('https://randomuser.me/api');
+    let json = await promise.json();
+    let person = new Person(json.results[0]);
+    return person;
+}
+
+
+async function showPeorson(){
+    let person = await getInformAboutPerson();
+    let personBlock = document.createElement('div');
+    personBlock.className = "personDiv";
+
+    let image = document.createElement('img');
+    image.src = person.image;
+    personBlock.append(image);
+
+    let cell = document.createElement('p');
+    cell.innerHTML = "Cell: " +person.cell;  
+    personBlock.append(cell);
+
+    let city = document.createElement('p');
+    city.innerHTML = "City: " + person.city;    
+    personBlock.append(city);
+
+    let country = document.createElement('p');
+    country.innerHTML = "Country: " + person.country;    
+    personBlock.append(country);
+
+    let postcode = document.createElement('p');
+    postcode.innerHTML = "PostCode :"+ person.postcode;    
+    personBlock.append(postcode);
+
+    blockForPeople.append(personBlock);
+}
+
+async function generatePerson(num){
+    for(let i = 0; i < num; i++){
+        showPeorson();
+    }
+}
